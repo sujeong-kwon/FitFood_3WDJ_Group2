1a42b3875190bfdc93a35281d2ef500f69ba5827
Sez <bjw8594@naver.com>
2020-06-08 14:12:12 +0900

    회원가입 오류수정

--- Web/Laravel_Vue/app/Http/Controllers/SignupController.php
+++ Web/Laravel_Vue/app/Http/Controllers/SignupController.php
@@ -3,12 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use User;
 
 class SignupController extends Controller
 {
     public function signup(Request $req) {
         try {
             $test = DB::select('select * from users where user_email = ?', [$req->user_email]);
+
+            $user_activity_index = $req->user_activity_index;
+
+            if($user_activity_index = 25){
+                $user_activity_index = "활동이 적다";
+            }else if($user_activity_index = 30){
+                $user_activity_index = "평범하다";
+            }else if($user_activity_index = 35){
+                $user_activity_index = "활동적이다";
+            }else if($user_activity_index = 40){
+                $user_activity_index = "많이 활동적이다";
+            }
+
+            $user_gender = $req->user_gender;
+
+            if($user_gender = 1){
+                $user_gender = "남자";
+            }else if($user_gender = 2){
+                $user_gender = "여자";
+            }
 
             if ($test) {
                 return "False";
@@ -20,7 +44,8 @@ class SignupController extends Controller
                     'user_birthday'=>$req->user_birthday,
                     'user_height'=>$req->user_height,
                     'user_weight'=>$req->user_weight,
-                    'user_gender'=>$req->user_gender,
+                    'user_gender'=>$user_gender,
+                    'user_activity_index'=>$user_activity_index,
                     // 'user_character_image'=>$req->user_character_image,
                     // 'user_character_color'=>$req->user_character_color,
                 ]);
@@ -38,5 +63,7 @@ class SignupController extends Controller
         } catch(\Illuminate\Database\QueryException $e) {
             dd($e->getMessage());
         };
+
+        // return $req;
     }
 }
